20ba07d1267f1b33f4a22e9203dfe37ac2d6a25b
sochubert <sochubert@gmail.com>
2021-07-02 18:49:25 +0900

    footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Main' });
+});
+
+router.get('/introduction', function(req, res, next) {
+  res.render('introduction', { title: 'Introduction' })
+});
+
+router.get('/technology', function(req, res, next) {
+  res.render('technology', { title: 'Technology' })
+});
+
+router.get('/vision', function(req, res, next) {
+  res.render('vision', { title: 'Vision' })
+});
+
+router.get('/news', function(req, res, next) {
+  res.render('news', { title: 'News' })
+});
+
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' })
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' })
 });
 
 module.exports = router;
